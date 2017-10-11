990f921630a72a04f6c9d3c1fd4d97af39410105
Tait Takatani <tait.takatani@gmail.com>
2017-10-11 12:06:27 -0700

    added some README text and displays table for customer

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # bamazon
+
+A CLI based Amazon-like storefront with the MySQL and Node.js. The app will take in orders from customers and deplete stock from the store's inventory, track product sales across store's departments and then provide a summary of the highest-grossing departments in the store.
+
+## Getting Started
+
+### Prerequisites
+
+* Node.js
+* MySQL 
+
+### Installing
+
+1. Create Database, see example schema in bamazon.sql
+2. Install node dependencies
+
+```
+npm install
+```
+
+## Running the App
+
+### Customer Interface
+
+```
+node bamazonCustomer.js
+```
+
+### Manager Interface
+
+```
+node bamazonManager.js
+```
+
+### Supervisor Interface
+
+```
+node bamazonSupervisor.js
+```
+
+## Built With
+
+* [node.js](https://nodejs.org/en/) - Node Server
+* [mysqyl](https://www.mysql.com/) - Database Management
+* [inquirer-npm](https://www.npmjs.com/package/inquirer) - CLI Query
+* [cli-table-npm](https://www.npmjs.com/package/cli-table) - CLI Table Display
+
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 let inquirer = require('inquirer');
 let mysql = require('mysql');
+let Table = require('cli-table');
 
 let connection = mysql.createConnection({
   host: '127.0.0.1',
@@ -10,18 +11,29 @@ let connection = mysql.createConnection({
 });
 
 let queryCust = function () {
-  inquirer.prompt([
-    {
-      name: 'id',
-      message: 'ID of item?'
-    }, {
-      name: 'quant',
-      message: 'Quantity to purchase?'
-    }
-  ]).then(function (ans) {
-    connection.connect(function (err) {
+  connection.connect(function (err) {
+    if (err) throw err;
+    connection.query(`SELECT * FROM products`, function (err, res) {
       if (err) throw err;
-      purchaseItem(ans.id, ans.quant);
+      let table = new Table({
+        head: ['ID', 'PRODUCT', 'PRICE'],
+        colWidths: [5, 25, 10]
+      });
+      for (let i = 0; i < res.length; i++) {
+        table.push([res[i].item_id, res[i].product_name, res[i].price]);
+      }
+      console.log(table.toString());
+      inquirer.prompt([
+        {
+          name: 'id',
+          message: 'ID of item to purchase?'
+        }, {
+          name: 'quant',
+          message: 'Quantity to purchase?'
+        }
+      ]).then(function (ans) {
+        purchaseItem(ans.id, ans.quant);
+      });
     });
   });
 };
@@ -44,7 +56,7 @@ let purchaseItem = function (id, quant) {
           WHERE item_id=${id}`,
           function (err, res) {
             if (err) throw err;
-            console.log(`TOTAL COST: $${totalCost}`);
+            console.log(`TOTAL COST: $${totalCost.toFixed(2)}`);
             connection.end();
           }
         );
